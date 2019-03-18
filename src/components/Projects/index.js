bd628fe4abceb3d9959f00b7787c9e9ac66b72a7
Silvia Feng <z3375641@student.unsw.edu.au>
2019-03-18 16:50:02 +1100

    target new tab for links

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -6,7 +6,7 @@ const Projects = ({children}) => {
         <div className="projects" id="projects">  
             <h2 className="text-projects">Check out some of my other work!</h2>
             <div className="projects-items">
-                <a href="https://goofy-beaver-c88cd1.netlify.com/" className="projects-items__box">
+                <a href="https://goofy-beaver-c88cd1.netlify.com/" target="_blank" className="projects-items__box">
                     <div className="project-items__background project-items__background--memorise"></div>
                     <div className="project__text">
                         <h3 className="project__text--heading">Memory Game</h3>
@@ -17,7 +17,7 @@ const Projects = ({children}) => {
                         </ul>
                     </div>
                 </a>
-                <a href="https://www.streamjokes.com/" className="projects-items__box" >
+                <a href="https://www.streamjokes.com/" target="_blank" className="projects-items__box" >
                     <div className="project-items__background project-items__background--stream-jokes"></div>
                     <div className="project__text">
                         <h3 className="project__text--heading">StreamJokes</h3>
